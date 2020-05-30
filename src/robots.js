0fee9db5d5223a2ce8e716211142a82adc77ef2d
grzlz <guillermo.rdzlpz@gmail.com>
2020-05-30 02:03:14 -0500

    dploy

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+ {
+ 	id: 1,
+ 	name: 'Leanne Graham',
+ 	username: 'Bret',
+ 	email: 'sincere@april.biz'
+ },
+ {
+ 	id: 2,
+ 	name: 'Ervin Howell',
+ 	username: 'Antonette',
+ 	email: 'sadfa@gmail.com'
+ },
+ {
+ 	id: 3,
+ 	name: 'Blair Battaglia',
+ 	username: 'Cali',
+ 	email: 'upqad@gmail.com'
+ },
+ {
+ 	id: 4,
+ 	name: 'Kiko',
+ 	username: 'Bernie',
+ 	email: 'saf@gmail.com'
+ },
+{
+	id: 5,
+	name: 'Capitan Morgan',
+	username: 'CaptMG',
+	email: 'adsf@gmail.com'
+},
+{
+	id: 6,
+	name: 'Mariana',
+	username: 'Gato21',
+	email: 'amqp@gmail.com'
+},
+{
+	id: 7,
+	name: 'Vicente',
+	username: 'Fernandez',
+	email: 'vf@gmail.com'
+},
+{
+	id: 8,
+	name: 'Alexis',
+	username: 'Sanchez',
+	email: 'aS@gmail.com'
+},
+{
+	id: 9,
+	name: 'Ervin Howell',
+	username: 'Bret',
+	email: 'asdfwp@gmail.com'
+},
+{
+	id: 10,
+	name: 'Chelsey Dietrich',
+	username: 'Kamren',
+	email: 'amdp@gmai.com'
+}
+]
